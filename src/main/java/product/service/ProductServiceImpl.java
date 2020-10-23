184e8c0c2de041812dca61d6747a60fa5e1ed95f
ssherstnov <ssherstnov@mmtr.ru>
2020-10-23 18:00:05 +0300

    Reformat ProductServiceImpl code

--- src/main/java/product/service/ProductServiceImpl.java
+++ src/main/java/product/service/ProductServiceImpl.java
@@ -17,130 +17,117 @@ import product.repository.ProductRepository;
 @Service
 @PropertySource(value = "classpath:/project.properties")
 public class ProductServiceImpl implements ProductService {
-	
-	@Autowired
-	private ProductRepository productRepository;
-	
-	@Value("${project.basedir}")
-	String basedir;
-	
-	@Override
-	@Transactional
-	public void save(String title, double price, MultipartFile imageFile) {
-		
-		final String SAVE_LOCATION = basedir + "/src/main/webapp/productImages/";
-		
-		String fileName = imageFile.getOriginalFilename();
-		String location = SAVE_LOCATION;
-        
-		File pathFile = new File(location + fileName);
-        
-		if (!pathFile.exists()) {
-		    	try {
-		    	    	imageFile.transferTo(pathFile);
-		    	} 
-		    	catch (IOException e) {
-		    	    	e.printStackTrace();
-		    	}
-		}
-        
-		else {
-        	
-		    	String tempName = fileName;
-        	
-		    	for (int i = 1; pathFile.exists(); i++) {
-		    	    	fileName = "(" + i + ")" + tempName;
-		    	    	pathFile = new File(location + fileName);
-		    	}
-		    	
-		    	try {
-		    	    	imageFile.transferTo(pathFile);
-		    	} 
-		    	catch (IOException e) {
-		    	    	e.printStackTrace();
-		    	}
-		}
- 
-		Product product = new Product(title, price, fileName);
-		productRepository.save(product);
-	}
-	
-	@Override
-	@Transactional
-	public void edit(int id, String title, double price, MultipartFile imageFile) {
-		
-		final String SAVE_LOCATION = basedir + "/src/main/webapp/productImages/";
-		
-		String location = SAVE_LOCATION;
-		
-		Product product = getById(id);
-		
-		String oldFileName = product.getImage();
-		File deletePathFile = new File(location + oldFileName);
-		deletePathFile.delete();
-		
-                String fileName = imageFile.getOriginalFilename();
-                
-                File pathFile = new File(location + fileName);
-                
-                if (!pathFile.exists()) {
-                    
-                	try {
-                	    	imageFile.transferTo(pathFile);
-                	} 
-                	catch (IOException e) {
-                	    	e.printStackTrace();
-                	}
-                }
-                
-                else {
-                	
-                	String tempName = fileName;
-                	
-                	for (int i = 1; pathFile.exists(); i++) {
-                		fileName = "(" + i + ")" + tempName;
-                		pathFile = new File(location + fileName);
-                	}
-                	
-                	try {
-                	    	imageFile.transferTo(pathFile);
-                  	} 
-                  	catch (IOException e) {
-                  	    	e.printStackTrace();
-                  	}
-                }
-        		
-                product.setTitle(title);
-                product.setPrice(price);
-                product.setImage(fileName);
-        	productRepository.edit(product);
-        }
-	
-	@Override
-	@Transactional
-	public void delete(int id) {
 
-		final String SAVE_LOCATION = basedir + "/src/main/webapp/productImages/";
-		
-		String location = SAVE_LOCATION;
-		
-		Product product = getById(id);
-		String fileName = product.getImage();
-		File pathFile = new File(location + fileName);
-		pathFile.delete();
-		productRepository.delete(product);
-	}
-	
-	@Override
-	@Transactional
-	public List<Product> getAll() {
-		return productRepository.getAll();
-	}
-	
-	@Override
-	@Transactional
-	public Product getById(int id) {
-		return productRepository.getById(id);
-	}
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Value("${project.basedir}")
+    private String basedir;
+
+    @Override
+    @Transactional
+    public void save(String title, double price, MultipartFile imageFile) {
+
+        final String SAVE_LOCATION = basedir + "/src/main/webapp/productImages/";
+
+        String fileName = imageFile.getOriginalFilename();
+
+        File pathFile = new File(SAVE_LOCATION + fileName);
+
+        if (!pathFile.exists()) {
+            try {
+                imageFile.transferTo(pathFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+
+            String tempName = fileName;
+
+            for (int i = 1; pathFile.exists(); i++) {
+                fileName = "(" + i + ")" + tempName;
+                pathFile = new File(SAVE_LOCATION + fileName);
+            }
+
+            try {
+                imageFile.transferTo(pathFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Product product = new Product(title, price, fileName);
+        productRepository.save(product);
+    }
+
+    @Override
+    @Transactional
+    public void edit(int id, String title, double price, MultipartFile imageFile) {
+
+        final String SAVE_LOCATION = basedir + "/src/main/webapp/productImages/";
+
+        Product product = getById(id);
+
+        String oldFileName = product.getImage();
+        File deletePathFile = new File(SAVE_LOCATION + oldFileName);
+        deletePathFile.delete();
+
+        String fileName = imageFile.getOriginalFilename();
+
+        File pathFile = new File(SAVE_LOCATION + fileName);
+
+        if (!pathFile.exists()) {
+
+            try {
+                imageFile.transferTo(pathFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+
+            String tempName = fileName;
+
+            for (int i = 1; pathFile.exists(); i++) {
+                fileName = "(" + i + ")" + tempName;
+                pathFile = new File(SAVE_LOCATION + fileName);
+            }
+
+            try {
+                imageFile.transferTo(pathFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        product.setTitle(title);
+        product.setPrice(price);
+        product.setImage(fileName);
+        productRepository.edit(product);
+    }
+
+    @Override
+    @Transactional
+    public void delete(int id) {
+
+        final String SAVE_LOCATION = basedir + "/src/main/webapp/productImages/";
+
+        Product product = getById(id);
+        String fileName = product.getImage();
+        File pathFile = new File(SAVE_LOCATION + fileName);
+        pathFile.delete();
+        productRepository.delete(product);
+    }
+
+    @Override
+    @Transactional
+    public List<Product> getAll() {
+        return productRepository.getAll();
+    }
+
+    @Override
+    @Transactional
+    public Product getById(int id) {
+        return productRepository.getById(id);
+    }
 
 }
