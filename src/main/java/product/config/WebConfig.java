958cca4dce2462e8e0db5fd6ed203747379be278
ssherstnov <sherstnovsn@yandex.ru>
2020-10-28 15:57:20 +0300

    Reformat WebConfig code

--- src/main/java/product/config/WebConfig.java
+++ src/main/java/product/config/WebConfig.java
@@ -14,27 +14,27 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 @EnableWebMvc
 @ComponentScan(basePackages = "product")
 public class WebConfig implements WebMvcConfigurer {
-	
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    		registry.addResourceHandler("/style/**").addResourceLocations("/style/");
-    		registry.addResourceHandler("/images/**").addResourceLocations("/images/");
-    		registry.addResourceHandler("/productImages/**").addResourceLocations("/productImages/");
-        }
-	
-	@Bean
-	public ViewResolver viewResolver(){
-                InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-                resolver.setPrefix("/WEB-INF/pages/");
-                resolver.setSuffix(".jsp");
-                return resolver;
-	}
-	
-	@Bean(name = "multipartResolver")
-	public CommonsMultipartResolver multipartResolver() {
-	    	CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-	    	multipartResolver.setMaxUploadSize(1000000);
-	    	return multipartResolver;
-	}
-	
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/style/**").addResourceLocations("/style/");
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/productImages/**").addResourceLocations("/productImages/");
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(1000000);
+        return multipartResolver;
+    }
+
 }
