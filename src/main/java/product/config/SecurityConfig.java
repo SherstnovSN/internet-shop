796c4f57f9542ba85edaf3d8ab74105bcfff2799
Sergey Sherstnov <sherstnovsn@yandex.ru>
2020-11-10 23:21:51 +0300

    Reformat SecurityConfig code

--- src/main/java/product/config/SecurityConfig.java
+++ src/main/java/product/config/SecurityConfig.java
@@ -16,41 +16,37 @@ import product.service.UserDetailsServiceImpl;
 @EnableWebSecurity
 @ComponentScan(basePackages = "product")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-        @Autowired
-        private UserDetailsServiceImpl userDetailsService;
-    
-        @Autowired
-        public void registerGlobalAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-    	auth
-            	.userDetailsService(userDetailsService)
-                .passwordEncoder(getBCryptPasswordEncoder());
-        }
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
-		http.authorizeRequests()
-				.antMatchers("/product", "/add-new-product", "/edit-product/*", "/delete-product/*").access("hasAnyAuthority('ADMIN', 'MODERATOR')")
-				.antMatchers("/users").access("hasAuthority('ADMIN')").and()
-			.formLogin()
-				.loginPage("/login")
-				.loginProcessingUrl("/j_spring_security_check")
-				.failureUrl("/login?error")
-				.usernameParameter("j_username").passwordParameter("j_password")
-				.permitAll().and()
-			.logout()
-				.permitAll()
-				.logoutUrl("/logout")
-				.logoutSuccessUrl("/")
-				.invalidateHttpSession(true)
-				.deleteCookies("JSESSIONID").and()
-			.csrf().disable();
-		
-	}
-	
-	@Bean
-	public BCryptPasswordEncoder getBCryptPasswordEncoder(){
-	    	return new BCryptPasswordEncoder();
-	}
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public void registerGlobalAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(getBCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        	.antMatchers("/product", "/add-new-product", "/edit-product/*", "/delete-product/*").access("hasAnyAuthority('ADMIN', 'MODERATOR')")
+            .antMatchers("/users").access("hasAuthority('ADMIN')").and()
+            .formLogin()
+            .loginPage("/login")
+            .loginProcessingUrl("/j_spring_security_check")
+            .failureUrl("/login?error")
+            .usernameParameter("j_username").passwordParameter("j_password")
+            .permitAll().and()
+            .logout()
+            .permitAll()
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/")
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID").and()
+            .csrf().disable();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder getBCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
