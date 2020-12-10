a3589ce1747d074f47a53efd0694a0899fe570bd
ssherstnov <sherstnovsn@yandex.ru>
2020-12-10 18:03:04 +0300

    Excluded WebConfig class from ComponentScan annotations of other config classes

--- src/main/java/product/config/SecurityConfig.java
+++ src/main/java/product/config/SecurityConfig.java
@@ -4,17 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import product.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-@ComponentScan(basePackages = "product")
+@ComponentScan(basePackages = "product", excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
